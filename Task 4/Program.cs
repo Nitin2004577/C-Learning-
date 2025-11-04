0e3349522a89e3cd5b4f00a119bfb121e1777614
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-11-04 10:33:13 +0545

    Task 4 Complete

---
+++ Task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] favNums = [1,2,3,4,5];
+
+//sorting using array.sort();
+Array.Sort(favNums);
+
+//reversing the sorted array
+Array.Reverse(favNums);
+
+//printing each element in the array using a for loop
+System.Console.WriteLine("Printing each element in the array: ");
+for (int i = 0; i < favNums.Length; i++)
+{
+    System.Console.WriteLine(favNums[i]);
+}
+
+// finding position of an element in the array using .IndexoF()
+
+int targetNum = 3;
+
+System.Console.WriteLine($"The index of the target number: {targetNum} is {Array.IndexOf(favNums, targetNum)}");
