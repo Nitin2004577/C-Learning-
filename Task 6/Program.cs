90416a9fe5b3ff4360bea9c00d58b8c69999f29a
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-11-04 10:37:26 +0545

    Task 6 complete

---
+++ Task 6/Program.cs
@@ -0,0 +1,30 @@
+﻿//Create a List<string> containing the name of 3 of your favourite fruits
+
+List<string> favFruits = new List<string>(){"apple", "banana", "Mango"};
+
+//adding a new fruit to the list
+favFruits.Add("Papaya");
+
+//remove one fruit from the list
+favFruits.Remove("banana");
+
+//printig all the fruits using a foreach loop
+foreach (string favFruit in favFruits)
+{
+    System.Console.WriteLine(favFruit);
+}
+
+//creating a dictionary<int, string>
+Dictionary<int, string> fruits = new Dictionary<int, string>()
+{
+    { 1, "apple" },
+    { 2, "banana" }
+};
+//adding a new entry to the dictionary
+fruits.Add(3, "peach");
+
+//printing all the key value pairs
+foreach (KeyValuePair<int, string> fruit in fruits)
+{
+    System.Console.WriteLine($"ID: {fruit.Key}, Name: {fruit.Value}");
+}
